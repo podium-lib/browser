55dde98aef6447a4bfedaf48d7a26fd6cf4b19c2
Alexander Bjerkan <alexander.bjerkan@gmail.com>
2019-11-11 14:19:31 +0100

    add ts definitions

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+export class Event {
+    channel: string;
+    topic: string;
+    payload?: unknown;
+}
+
+export class MessageBus {
+    constructor();
+
+    publish(channel: string, topic: string, payload?: unknown): void;
+
+    subscribe(
+        channel: string,
+        topic: string,
+        callback: (event: Event) => void,
+    ): void;
+
+    peek(channel: string, topic: string): Event | void;
+
+    log(channel: string, topic: string): Event[];
+}
