e62f01fdfd8668c20a23ad660be46d56d5eedeb1
Trygve Lie <trygve.lie@finn.no>
2021-09-29 16:22:00 +0200

    Restructure dual module type package

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,17 @@ import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 
-export default [{
+export default {
     input: 'src/index.js',
-    output: [{
-        dir: 'dist/cjs',
-        format: 'cjs',
-    }],
     plugins: [resolve(), commonjs(), getBabelOutputPlugin({
         presets: ['@babel/preset-env']
     })],
-    preserveModules: true,
-},{
-    input: 'src/index.js',
-    output: [{
-        dir: 'dist/esm',
-        format: 'esm',
-    }],
-    plugins: [resolve(), commonjs()],
-    preserveModules: true,
-}];
+    output: [
+        {
+            exports: 'auto',
+            format: 'cjs',
+            dir: 'dist/',
+            preserveModules: true,
+        }
+    ],
+};
